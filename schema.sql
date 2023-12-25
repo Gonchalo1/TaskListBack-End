3f8fe267afcaa985f7cb1a9aa22924c435982972
G <gonzalocal127@gmail.com>
2023-12-25 19:10:00 -0300

    Pulling with data the database

--- schema.sql
+++ schema.sql
@@ -1,40 +1,39 @@
-create database todo_tutorial
 
-use todo_tutorial;
+-- Usamos todo_tutorial como base de datos
+\c todo_tutorial
 
-CREATE TABLE users(
-id INT AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(255),
-email VARCHAR(255) UNIQUE NOT NULL,
-password VARCHAR(255)
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255)
 );
 
-CREATE TABLE todos(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE todos (
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     completed BOOLEAN DEFAULT false,
     user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-
-CREATE TABLE shared_todos(
-    id INT SERIAL PRIMARY KEY,
+CREATE TABLE shared_todos (
+    id SERIAL PRIMARY KEY,
     todo_id INT,
     user_id INT,
     shared_with_id INT,
-    FOREIGN KEY (todo_id) REFERENCES todos(id) ON DELETE CASCADE
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (todo_id) REFERENCES todos(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (shared_with_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-INSERT INTO users(name,email,password) VALUES('Beto', 'user1@example.com', 'password');
-INSERT INTO users(name,email,password) VALUES('Alberto', 'user2@example.com', 'password');
+INSERT INTO users (name, email, password) VALUES
+('Beto', 'user1@example.com', 'password'),
+('Alberto', 'user2@example.com', 'password');
 
-
-INSERT INTO todos(title, user_id) VALUES
-("🏃‍♂️ Go for a morning run",1),
-("💻 Work on proyect presentation",1),
+INSERT INTO todos (title, user_id) VALUES
+('🏃‍♂️ Go for a morning run', 1),
+('💻 Work on project presentation', 1),
 ("🛒 Go grocery shopping",1),
 ("📕 Read 30 pages of book",1),
 ("🚲 Ride bike on the park",1),
@@ -44,7 +43,11 @@ INSERT INTO todos(title, user_id) VALUES
 ("🛏 Clean the house",1),
 ("😴 Sleep for 8 hours",1),
 
-SELECT todos.*, shared_todos.shared_with_id
+INSERT INTO shared_todos (todo_id, user_id, shared_with_id) VALUES
+(1, 1, 2),
+-- Inserta otros registros aquí
+
+(SELECT todos.id, shared_todos.shared_with_id
 FROM todos
 LEFT JOIN shared_todos ON todos.id = shared_todos.todo_id
-WHERE todos.user_id = [user_id] OR shared_todos.shared_with_id
+WHERE todos.user_id = 1 OR shared_todos.shared_with_id IS NOT NULL)
